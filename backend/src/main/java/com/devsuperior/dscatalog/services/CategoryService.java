27512199e7091349b3a3e0ee9980e5c218f8db16
tlrmail@gmail.com <tlrmail@gmail.com>
2026-08-03 16:16:09 -0300

    DTO

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dscatalog.dtos.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
 
@@ -17,8 +18,9 @@ public class CategoryService {
 	private CategoryRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<Category> findAll(){
-		List<Category> categories = repository.findAll();
-		return categories;
+	public List<CategoryDTO> findAll(){
+		List<Category> list = repository.findAll();
+		List<CategoryDTO> dtos = list.stream().map(category -> new CategoryDTO(category)).toList();
+		return dtos;
 	}
 }
